c7bcd4942c124d4e58fdc1fdb309a098e13ed36f
Melisa <melisahrnn@gmail.com>
2023-06-02 23:11:29 -0400

    Java Files Uploaded

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    //Attributes
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 50000.00;
+
+    //Methods: drive and brake
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Car ourCar = new Car();
+
+        /*Process the attributes
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+        System.out.println();//Space
+        System.out.println(ourCar.make);
+        System.out.println(ourCar.model);*/
+
+        /*Process the methods
+        myCar.drive();
+        myCar.brake();*/
+    }
+}
